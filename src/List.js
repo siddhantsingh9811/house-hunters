2e65307aaa3fd949a8fe87b3a563863d81b789c5
Siddhant Singh <ssiddhant9811@gmail.com>
2022-10-20 23:34:27 +0530

    List component structure completed

--- src/List.js
+++ src/List.js
@@ -1,5 +1,17 @@
-const List = () => {
-    return (  );
-}
- 
-export default List;
+const List = ({ houses, title, handleDelete }) => {
+    return (
+      <div className="list">
+        <h2>{ title }</h2>
+        {houses.map(house => (
+          <div className="list-preview" key={house.id} >
+            <h2>{ house.name } - { house.bhk } BHK</h2>
+            <p><span className="t">Location:</span> { house.location }</p>
+            <p><span className="t">Price:</span> { house.price }</p>
+            <button onClick={() => handleDelete(house.id)}>Delete Entry</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+   
+  export default List;
