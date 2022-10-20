21096d6f6a2e76ac54e5719251097c4346225295
Vidhant007 <thapavidhant@gmail.com>
2022-10-20 23:31:14 +0530

    Home page structure defined

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,23 @@
+import List from "./List";
+
 const Home = () => {
-    return (  );
+  
+  const [houses, setHouses] = useState([
+    { name: 'Apartment A', location: 'Delhi', bhk: 3, price:"1L", id: 1 },
+    { name: 'Apartment B', location: 'Dehradun', bhk: 2,price:"2L", id: 2 },
+    { name: 'Apartment C', location: 'Bangalore', bhk: 3,price:"1L", id: 3 }
+  ])
+const Home = () => {
+    return ( 
+        <div className="home">
+        <List houses={houses} title="Available Houses" handleDelete={handleDelete} />
+        <div className="overlay">
+            <div className="box">
+                
+            </div>
+        </div>
+        </div>
+     );
 }
  
 export default Home;
