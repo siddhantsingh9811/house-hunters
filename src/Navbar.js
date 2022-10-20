29b8d3bfcdc48815e9f23bcb43aaeb3e8d41bddb
keshavbhrdwaj7 <110217847+keshavbhrdwaj7@users.noreply.github.com>
2022-10-20 23:27:56 +0530

    Navbar Structure and Fonts Added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,14 @@
 const Navbar = () => {
-    return (  );
+
+  return (
+    <nav className="navbar">
+      <h1>🏠 House Hunters</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <span onClick={() => handleOverlay()}>New Entry</span>
+      </div>
+    </nav>
+  );
 }
  
 export default Navbar;
